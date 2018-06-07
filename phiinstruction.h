b412eece51421d4fce1bf958e602199a94ad0097
Mr_Little_Kitty <nuclearcat1337@gmail.com>
2018-06-07 14:33:12 -0700

    Add method to create instructions from phi instruction at runtime

--- phiinstruction.h
+++ phiinstruction.h
@@ -10,6 +10,38 @@ private:
     int lhsVariable;
     std::map<int, int> *rhsVariableMap;
 
+    Instruction *createStoreInstruction(int varNumber, int bytecodeIndex) {
+        if (varNumber == 0)
+            return new Instruction(bytecodeIndex, op_istore_0);
+        else if (varNumber == 1)
+            return new Instruction(bytecodeIndex, op_istore_1);
+        else if (varNumber == 2)
+            return new Instruction(bytecodeIndex, op_istore_2);
+        else if (varNumber == 3)
+            return new Instruction(bytecodeIndex, op_istore_3);
+        else {
+            std::vector<unsigned char> operands;
+            operands.emplace_back((unsigned char) varNumber);
+            return new Instruction(bytecodeIndex, op_istore, operands);
+        }
+    }
+
+    Instruction *createLoadInstruction(int varNumber, int bytecodeIndex) {
+        if (varNumber == 0)
+            return new Instruction(bytecodeIndex, op_iload_0);
+        else if (varNumber == 1)
+            return new Instruction(bytecodeIndex, op_iload_1);
+        else if (varNumber == 2)
+            return new Instruction(bytecodeIndex, op_iload_2);
+        else if (varNumber == 3)
+            return new Instruction(bytecodeIndex, op_iload_3);
+        else {
+            std::vector<unsigned char> operands;
+            operands.emplace_back((unsigned char) varNumber);
+            return new Instruction(bytecodeIndex, op_iload, operands);
+        }
+    }
+
 public:
     PhiInstruction(int variable) : Instruction(-1, op_phi), lhsVariable(variable), originalLHS(variable) {
         rhsVariableMap = new std::map<int, int>();
@@ -42,6 +74,15 @@ public:
     std::map<int, int> *viewRHSMap() {
         return rhsVariableMap;
     };
+
+    std::vector<Instruction *> getInstructions(int fromBlock) {
+        std::vector<Instruction *> result;
+        if (rhsVariableMap->count(fromBlock) > 0) {
+            result.emplace_back(createLoadInstruction((*rhsVariableMap)[fromBlock], getByteCodeIndex()));
+            result.emplace_back(createStoreInstruction(getCurrentLHS(), getByteCodeIndex()));
+        }
+        return result;
+    }
 };
 
 #endif //JAVACOMPILER_PHIINSTRUCTION_H
